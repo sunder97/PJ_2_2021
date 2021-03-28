205d6a44a6f83ac548a94d9dbea3843cea10f1d8
sunder97 <sunder97@naver.com>
2021-03-28 19:35:21 +0900

    UI2 and Sound

---
+++ PJ_Davinci/Assets/Script/SceneMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SingleChange()
+    {
+        SceneManager.LoadScene("SingleGameScene");
+    }
+    public void DoubleChange()
+    {
+        SceneManager.LoadScene("DoubleGameScene");
+    }
+}
---
+++ PJ_Davinci/Assets/Script/Sound.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Option : MonoBehaviour
+{
+    public Slider BGMslider, SEslider;
+    public AudioSource bgm, se;
+
+    private float bgmvolGage = 1f;
+    private float sevolGage = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bgmvolGage = PlayerPrefs.GetFloat("bgmvolGage", 1f);
+        BGMslider.value = bgmvolGage;
+        bgm.volume = BGMslider.value;
+
+        sevolGage = PlayerPrefs.GetFloat("sevolGage", 1f);
+        SEslider.value = sevolGage;
+        se.volume = SEslider.value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BGMSliderF();
+        SESliderF();
+    }
+
+    public void BGMSliderF()
+    {
+        bgm.volume = BGMslider.value;
+        bgmvolGage = BGMslider.value;
+        PlayerPrefs.SetFloat("bgmvolGage", 1f);
+    }
+
+    public void SESliderF()
+    {
+        se.volume = SEslider.value;
+        sevolGage = SEslider.value;
+        PlayerPrefs.SetFloat("sevolGage", 1f);
+    }
+}
